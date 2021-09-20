c3b269debc42e86815c7b4adc63f5f9647d999ba
EvgeniiSamarin <evgeniy.samarin@flatstack.com>
2021-09-20 18:50:59 +0300

    Initital commit

---
+++ MethodDispatch/MethodDispatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MethodDispatch
+//
+//  Created by Евгений Самарин on 20.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
